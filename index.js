9f951ca1af13a72eec87247a699e427c2d161ebf
Carlo Cassinari <carlo.cassinari@gmail.com>
2014-05-09 09:07:35 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,110 @@
+module.exports.predict = function(config){
+	/*
+
+		Neural network test and prediction algorithm
+		Carlo Cassinari 29/04/2014
+
+		suggestions from: http://stats.stackexchange.com/questions/10162/how-to-apply-neural-network-to-time-series-forecasting
+		brain library from: https://github.com/harthur/brain
+
+		onther method: http://www.kadbox.it/Images/Articoli_PMI/PMI%200807%20-%20Strumenti%20di%20studio%20del%20portfolio%20prodotti%20-%20prevedere%20le%20vendite%20di%20un%20prodotto%20-%2045_53.pdf
+
+
+	*/
+
+	var brain = require("brain");
+	var net = new brain.NeuralNetwork({
+	   //hiddenLayers: [4,3,2,1],
+	   learningRate: 0.01
+	});
+
+	// max steps to check
+	var checkSteps = config.checkSteps ;
+	// max steps to predict
+	var predictionSteps = config.predictionSteps ;
+	// steps per pattern
+	var step = config.step ;
+	// serie value
+	var serie = config.serie ;
+
+	var maxValue = 0 ;
+
+	for (var i=0;i<serie.length;i++){
+		if (maxValue < serie[i]) maxValue = serie[i] ;
+	}
+
+
+	var training = [] ;
+	for (var i=0;i<serie.length-step;i=i+1){
+		var input = [] ;
+		for (var ii=0;ii<step;ii++){
+			var value = serie[ii+i] / maxValue ;
+			input.push(value) ;
+		}
+		
+		var trainingObj = {
+			input: input,
+			output: [serie[i+step]/maxValue]
+		}
+		training.push(trainingObj) ;
+	}
+
+	var trainOutput = net.train(training, {
+	  errorThresh: 0.001,  // error threshold to reach
+	  iterations: 20000,   // maximum training iterations
+	  log: false,           // console.log() progress periodically
+	  logPeriod: 10        // number of iterations between logging
+	}) ;
+	
+	if (config.debug) console.log(trainOutput) ;
+
+	var totError = 0 ;
+	var start = serie.length - checkSteps - 1 ;
+	var iter = 0 ;
+	for (var i=start;i<serie.length-step;i=i+1){
+		iter++ ;
+		var input = [] ;
+		//var visualInput = [] ;
+		for (var ii=0;ii<step;ii++){
+			var value = serie[ii+i] / maxValue ;
+			input.push(value) ;
+			//visualInput.push(serie[ii+i]) ;
+		}
+		var expectedoutput = serie[i+step] ;
+		var output = net.run(input) ;
+		var prediction = output[0] * maxValue ;
+		var error = Math.abs((prediction - expectedoutput))/expectedoutput ;
+		totError += error ;
+		//console.log(visualInput) ;
+		//console.log("ExpectedOuput: " + expectedoutput) ;
+		//console.log("Prediction: " + prediction) ;
+		//console.log("Error: " + error + "\n") ;
+	}
+
+
+
+	var start = serie.length - step ;
+	var maxStep = serie.length + predictionSteps - step ;
+	var predictionSerie = [] ;
+	for (var i=start;i<maxStep;i=i+1){
+		var input = [] ;
+		//var visualInput = [] ;
+		for (var ii=0;ii<step;ii++){
+			var value = serie[ii+i] / maxValue ;
+			input.push(value) ;
+			//visualInput.push(serie[ii+i]) ;
+		}
+		var output = net.run(input) ;
+		var prediction = Math.round(output[0] * maxValue) ;
+		//console.log(visualInput) ;
+		//console.log("Prediction: " + prediction + "\n") ;
+		serie.push(prediction) ;
+		predictionSerie.push(prediction) ;
+	}
+
+	return {
+		prediction: predictionSerie,
+		meanerror : totError/iter
+	}
+
+}
---
+++ predict-example.js
@@ -0,0 +1,41 @@
+/*
+
+	Neural network test and prediction algorithm
+	Carlo Cassinari 29/04/2014
+
+	suggestions from: http://stats.stackexchange.com/questions/10162/how-to-apply-neural-network-to-time-series-forecasting
+	brain library from: https://github.com/harthur/brain
+
+	onther method: http://www.kadbox.it/Images/Articoli_PMI/PMI%200807%20-%20Strumenti%20di%20studio%20del%20portfolio%20prodotti%20-%20prevedere%20le%20vendite%20di%20un%20prodotto%20-%2045_53.pdf
+
+
+*/
+
+var predict = require("../predict") ;
+
+var config = {
+	checkSteps : 16,
+	predictionSteps : 8,
+	step : 4,
+	serie : [
+		1198,2093,3274,1156,
+		1232,2112,3398,1285,
+		1297,2220,3876,1456,
+		1434,2473,4213,1506,
+		1602,2504,4594,1638,
+		1746,2672,4852,1932,
+		1978,2916,4976,2031,
+		2012,2944,4947,2194,
+		2213,3253,5033,2328,
+		1981,3015,4811,2058,
+		2017,2970,4768,2081,
+		2093,3067,4879,2116
+	]  
+}
+
+var prediction = predict.predict(config) ;
+
+console.log("PredictionSerie\n" + prediction.prediction + "\n");
+console.log("Mean error: " + prediction.meanerror) ;
+
+
